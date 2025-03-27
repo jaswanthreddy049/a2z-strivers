d7aaf8e5f2af220d726bc160a1eaf56e9b9c2d2b
Likitha Maddipati <jaswanthreddy1979@gmail.com>
2025-03-27 22:59:04 +0530

    add new file

--- 01_basics/05_recursion/05_sum of n numbers.py
+++ 01_basics/05_recursion/05_sum of n numbers.py
@@ -1,14 +1,14 @@
 """
-Given an integer n, calculate the sum of series 13 + 23 + 33 + 43 + … till n-th term.
+Given an integer n, calculate the sum of series 1^3 + 2^3 + 3^3 + 4^3 + … till n-th term.
 
 Examples:
 
 Input: n = 5
 Output: 225
-Explanation: 13 + 23 + 33 + 43 + 53 = 225
+Explanation: 1^3 + 2^3 + 3^3 + 4^3 + 5^3 = 225
 Input: n = 7
 Output: 784
-Explanation: 13 + 23 + 33 + 43 + 53 + 63 + 73 = 784
+Explanation: 1^3 + 2^3 + 3^3 + 4^3 + 5^3 + 6^3 + 7^3 = 784
 """
 class Solution:
     def sumOfSeries(self,n):
