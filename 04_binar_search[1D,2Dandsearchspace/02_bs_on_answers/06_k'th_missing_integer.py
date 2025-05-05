83635769c9f9a86cdd7145b157b9d0d40d852da2
Likitha Maddipati <jaswanthreddy1979@gmail.com>
2025-05-05 12:03:50 +0530

    add new file

---
+++ 04_binar_search[1D,2Dandsearchspace/02_bs_on_answers/06_k'th_missing_integer.py
@@ -0,0 +1,39 @@
+"""1539. Kth Missing Positive Number
+Solved
+Easy
+Topics
+Companies
+Hint
+Given an array arr of positive integers sorted in a strictly increasing order, and an integer k.
+
+Return the kth positive integer that is missing from this array.
+
+ 
+
+Example 1:
+
+Input: arr = [2,3,4,7,11], k = 5
+Output: 9
+Explanation: The missing positive integers are [1,5,6,8,9,10,12,13,...]. The 5th missing positive integer is 9.
+Example 2:
+
+Input: arr = [1,2,3,4], k = 2
+Output: 6
+Explanation: The missing positive integers are [5,6,7,...]. The 2nd missing positive integer is 6."""
+class Solution(object):
+    def findKthPositive(self, arr, k):
+        s=0  
+        e=len(arr)-1
+        while(s<=e):
+            mid=(s+e)//2
+            if arr[mid]-(mid+1)<k:
+                s=mid+1
+            else:e=mid-1
+        return s+k
+
+        """
+        :type arr: List[int]
+        :type k: int
+        :rtype: int
+        """
+        
