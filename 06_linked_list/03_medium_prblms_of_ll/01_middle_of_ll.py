fde135906690291fcb3ff20129c05ea81291430b
Likitha Maddipati <jaswanthreddy1979@gmail.com>
2025-05-10 11:15:22 +0530

    add new file

---
+++ 06_linked_list/03_medium_prblms_of_ll/01_middle_of_ll.py
@@ -0,0 +1,56 @@
+"""876. Middle of the Linked List
+Solved
+Easy
+Topics
+Companies
+Given the head of a singly linked list, return the middle node of the linked list.
+
+If there are two middle nodes, return the second middle node.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,3,4,5]
+Output: [3,4,5]
+Explanation: The middle node of the list is node 3.
+Example 2:
+
+
+Input: head = [1,2,3,4,5,6]
+Output: [4,5,6]
+Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one."""
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def middleNode(self, head):
+        cur=head
+        count=0
+        while(cur):
+            count+=1
+            cur=cur.next
+        if count%2==0:
+            slow=head
+            fast=head
+            while(fast):
+                slow=slow.next
+                fast=fast.next.next
+            return slow
+        else:
+            slow=head
+            fast=head
+            while(fast.next):
+                slow=slow.next
+                fast=fast.next.next
+            return slow
+            
+        
+        """
+        :type head: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        
