743159183581d2fb48ff9fd93466340d2252d040
Likitha Maddipati <jaswanthreddy1979@gmail.com>
2025-06-12 11:00:42 +0530

    new

---
+++ 13_binary_trees/11_iterative_postorder.py
@@ -0,0 +1,42 @@
+"""145. Binary Tree Postorder Traversal
+Solved
+Easy
+Topics
+premium lock icon
+Companies
+Given the root of a binary tree, return the postorder traversal of its nodes' values.
+
+ 
+
+Example 1:
+
+Input: root = [1,null,2,3]
+
+Output: [3,2,1]
+
+Explanation"""
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def postorderTraversal(self, root):
+        result=[]
+        stack=[root]
+        while(stack):
+            if not root:
+                return []
+            node=stack.pop()
+            result.append(node.val)
+            if node.left:
+                stack.append(node.left)
+            if node.right:
+                stack.append(node.right)
+        return result[::-1]
+        """
+        :type root: Optional[TreeNode]
+        :rtype: List[int]
+        """
+        
