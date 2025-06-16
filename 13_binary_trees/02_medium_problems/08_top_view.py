d329f1a54264e3be3b6b0848218edd7414422ee8
Likitha Maddipati <jaswanthreddy1979@gmail.com>
2025-06-16 14:44:26 +0530

    new

---
+++ 13_binary_trees/02_medium_problems/08_top_view.py
@@ -0,0 +1,35 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def rightSideView(self, root):
+        mydict={}
+        queue=deque([(root,0)])
+        minlen,maxlen=0,0
+        while(queue):
+            if not root:return []
+            node,col=queue.popleft()
+            minlen=min(minlen,col)
+            maxlen=max(maxlen,col)
+            if col not in mydict:
+                mydict[col]=node.val
+            if node.left:
+                queue.append([node.left,col-1])
+            if node.right:
+                queue.append([node.right,col+1])
+        stack=[]
+        for i in range(minlen,maxlen+1):
+            stack.append(mydict[i])
+        return stack
+
+
+        
+
+        """
+        :type root: Optional[TreeNode]
+        :rtype: List[int]
+        """
+        
